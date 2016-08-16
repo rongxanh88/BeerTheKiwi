c8d78de58611fb576414ca2b95b1acbce986d0ac
Bao Nguyen <rongxanh88@gmail.com>
2016-08-17 11:53:48 +1200

    Add Custom Data Loader - BreweryLoader class

---
+++ app/src/main/java/nguyenbao/beerthekiwi/BreweryLoader.java
@@ -0,0 +1,34 @@
+package nguyenbao.beerthekiwi;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Bao Nguyen on 8/17/2016.
+ */
+public class BreweryLoader extends AsyncTaskLoader<List<Brewery>> {
+
+    private String mUrl;
+    private static final String LOG_TAG = BreweryLoader.class.getName();
+
+    public BreweryLoader(Context context, String url) {
+        super(context);
+        mUrl = url;
+    }
+
+    @Override
+    public List<Brewery> loadInBackground() {
+
+        ArrayList<Brewery> breweries = FetchBreweryData.fetchBreweryData(mUrl);
+
+        return breweries;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+}
