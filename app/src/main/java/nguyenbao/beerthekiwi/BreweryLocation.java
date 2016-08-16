9d5a7172fc89bb44d71691ba2859c09c9709acea
Bao Nguyen <rongxanh88@gmail.com>
2016-08-17 10:17:21 +1200

    Add Brewery Location Class

---
+++ app/src/main/java/nguyenbao/beerthekiwi/BreweryLocation.java
@@ -0,0 +1,60 @@
+package nguyenbao.beerthekiwi;
+
+/**
+ * Created by Bao Nguyen on 8/17/2016.
+ */
+public class BreweryLocation {
+
+    //states of location
+    private String mStreetAdress;
+    private String mLocality;
+    private String mRegion;
+    private String mPostalCode;
+    private String mCountryName;
+    private double mLongitude;
+    private double mLatitude;
+
+    public BreweryLocation(){
+        //empty constructor
+    }
+
+    public BreweryLocation(String streetAddress, String locality, String region,String postalCode,
+                           String countryName, double longitude, double latitude){
+
+        mStreetAdress = streetAddress;
+        mLocality = locality;
+        mRegion = region;
+        mPostalCode = postalCode;
+        mCountryName = countryName;
+        mLongitude = longitude;
+        mLatitude = latitude;
+    }
+
+    public String getStreetAdress() {
+        return mStreetAdress;
+    }
+
+    public String getLocality() {
+        return mLocality;
+    }
+
+    public String getRegion() {
+        return mRegion;
+    }
+
+    public String getPostalCode() {
+        return mPostalCode;
+    }
+
+    public String getCountryName() {
+        return mCountryName;
+    }
+
+    public double getLongitude() {
+        return mLongitude;
+    }
+
+    public double getLatitude() {
+        return mLatitude;
+    }
+}
