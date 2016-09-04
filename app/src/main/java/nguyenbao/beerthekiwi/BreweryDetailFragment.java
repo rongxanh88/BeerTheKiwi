d7a3cd3e5efc9d9bd27aab84d68e50be01768167
Bao Nguyen <rongxanh88@gmail.com>
2016-09-05 11:59:10 +1200

    Add: ShareActionProvider for Brewery Detail

--- app/src/main/java/nguyenbao/beerthekiwi/BreweryDetailFragment.java
+++ app/src/main/java/nguyenbao/beerthekiwi/BreweryDetailFragment.java
@@ -8,17 +8,19 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.util.Pair;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
-
-import org.w3c.dom.Text;
 
 import java.io.InputStream;
 
@@ -28,6 +30,8 @@ public class BreweryDetailFragment extends Fragment {
 
     private static final String LOG_TAG = BreweryDetailFragment.class.getName();
     private static final String BASE_GEO_URI = "geo:";
+    private Brewery mSelectedBrewery;
+    private ShareActionProvider mShareActionProvider;
 
     public BreweryDetailFragment() {
         // Required empty public constructor
@@ -42,9 +46,9 @@ public class BreweryDetailFragment extends Fragment {
         setHasOptionsMenu(true);
 
         Bundle extras = getArguments();
-        final Brewery selectedBrewery = getBrewery(extras);
+        mSelectedBrewery = getBrewery(extras);
 
-        String imageUrl = selectedBrewery.getImages().getLargeUrl();
+        String imageUrl = mSelectedBrewery.getImages().getLargeUrl();
         if (imageUrl != null) {
             //find image view and set icon
             new DownloadImageTask((ImageView) rootview.findViewById(R.id.brewery_detail_image))
@@ -55,14 +59,14 @@ public class BreweryDetailFragment extends Fragment {
         }
 
         final TextView breweryName = (TextView)rootview.findViewById(R.id.brewery_name_detail_view);
-        breweryName.setText(selectedBrewery.getName());
+        breweryName.setText(mSelectedBrewery.getName());
 
         TextView breweryDescription = (TextView)rootview.findViewById(R.id.brewery_description_detail_view);
-        breweryDescription.setText(selectedBrewery.getDescription());
+        breweryDescription.setText(mSelectedBrewery.getDescription());
 
         //Viewgroup for location
-        final double latitude = selectedBrewery.getBreweryLocation().getLatitude();
-        final double longitude = selectedBrewery.getBreweryLocation().getLongitude();
+        final double latitude = mSelectedBrewery.getBreweryLocation().getLatitude();
+        final double longitude = mSelectedBrewery.getBreweryLocation().getLongitude();
         double invalid = FetchBreweryData.INVALID_VALUE;
 
         String firstCardinal;
@@ -78,15 +82,15 @@ public class BreweryDetailFragment extends Fragment {
             secondCardinal = "W";
         }
 
-        final String address = selectedBrewery.getBreweryLocation().getStreetAddress();
-        final String locality = selectedBrewery.getBreweryLocation().getLocality();
-        final String region = selectedBrewery.getBreweryLocation().getRegion();
+        final String address = mSelectedBrewery.getBreweryLocation().getStreetAddress();
+        final String locality = mSelectedBrewery.getBreweryLocation().getLocality();
+        final String region = mSelectedBrewery.getBreweryLocation().getRegion();
 
         ViewGroup locationView = (ViewGroup)rootview.findViewById(R.id.brewery_location_view_group);
         locationView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String brewery = selectedBrewery.getName();
+                String brewery = mSelectedBrewery.getName();
                 Uri locationByCoord = Uri.parse(BASE_GEO_URI + latitude + "," + longitude + "?")
                         .buildUpon()
                         .appendQueryParameter("q", address + ", " + locality + ", " + region + "(" + brewery + ")")
@@ -112,12 +116,12 @@ public class BreweryDetailFragment extends Fragment {
         }
 
         TextView breweryWebsite = (TextView)rootview.findViewById(R.id.brewery_website_detail_view);
-        breweryWebsite.setText("Website: " + selectedBrewery.getWebsite());
+        breweryWebsite.setText("Website: " + mSelectedBrewery.getWebsite());
         breweryWebsite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //create url string
-                String url = selectedBrewery.getWebsite();
+                String url = mSelectedBrewery.getWebsite();
 
                 //create intent
                 Intent i = new Intent(Intent.ACTION_VIEW);
@@ -127,9 +131,36 @@ public class BreweryDetailFragment extends Fragment {
         });
 
         TextView breweryDateOfEstablishment = (TextView)rootview.findViewById(R.id.brewery_date_of_establishment_detail_view);
-        breweryDateOfEstablishment.setText("Established: " + selectedBrewery.getDateOfEstablishment());
+        breweryDateOfEstablishment.setText("Established: " + mSelectedBrewery.getDateOfEstablishment());
 
         return rootview;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Inflate detail_share resource file.
+        inflater.inflate(R.menu.detail_share, menu);
+
+        // Locate MenuItem with ShareActionProvider
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+
+        // Fetch and store ShareActionProvider
+        mShareActionProvider = new ShareActionProvider(getContext());
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
+        if (mShareActionProvider != null){
+            mShareActionProvider.setShareIntent(createShareIntent());
+        }
+    }
+
+    private Intent createShareIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TITLE, mSelectedBrewery.getName());
+        shareIntent.putExtra(Intent.EXTRA_TEXT, mSelectedBrewery.getDescription());
+        shareIntent.putExtra(Intent.EXTRA_TEXT, mSelectedBrewery.getWebsite());
+
+        return shareIntent;
     }
 
     private Brewery getBrewery (Bundle extras){
